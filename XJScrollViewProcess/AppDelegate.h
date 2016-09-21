217e7de8ecf9e9bfacfd9cd46c957a2b2a24c33a
XJIMI <fn5128@gmail.com>
2016-09-21 11:06:01 +0800

    Initial Commit

---
+++ XJScrollViewProcess/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XJScrollViewProcess
+//
+//  Created by XJIMI on 2016/9/21.
+//  Copyright © 2016年 XJIMI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XJScrollViewProcess/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XJScrollViewProcess
+//
+//  Created by XJIMI on 2016/9/21.
+//  Copyright © 2016年 XJIMI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
